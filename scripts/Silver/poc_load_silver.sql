43423cc7fa2645c8ffa47d137a365692473e65d8
pushparajanigade02-dotcom <pushparaj.a.nigade02@gmail.com>
2025-11-06 19:47:40 +0530

    Create poc_load_silver.sql

---
+++ scripts/Silver/poc_load_silver.sql
@@ -0,0 +1,163 @@
+/*
+===============================================================================
+Stored Procedure: Load Silver Layer (Bronze -> Silver)
+===============================================================================
+Script Purpose:
+    This stored procedure performs the ETL (Extract, Transform, Load) process to 
+    populate the 'silver' schema tables from the 'bronze' schema.
+	Actions Performed:
+		- Replace Silver tables.
+		- Inserts transformed and cleansed data from Bronze into Silver tables.
+		
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    EXEC Silver.load_silver;
+===============================================================================
+*/
+
+CREATE OR REPLACE PROCEDURE silver.load_silver()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+
+	INSERT INTO silver.crm_cust_info (
+		  cst_id,
+		  cst_key,
+		  cst_firstname,
+		  cst_lastname,
+		  cst_marital_status,
+		  cst_gndr,
+		  cst_create_date)
+	
+	SELECT
+		  cst_id,
+		  cst_key,
+		  TRIM(cst_firstname) AS cst_firstname,
+		  TRIM(cst_lastname) AS cst_lastname,
+		  CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+		       WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+			   ELSE 'n/a'
+		  END cst_marital_status,
+		  CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+		       WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+			   ELSE 'n/a'
+		  END cst_gndr,
+		  cst_create_date
+	FROM (
+		SELECT
+			  *,
+			  ROW_NUMBER () OVER (PARTITION BY cst_id ORDER BY cst_create_date) AS flag_last
+		FROM bronze.crm_cust_info
+		WHERE cst_id IS NOT NULL
+	)WHERE flag_last = 1;
+	
+	-- Similarly we to do data insertation in remaining tables
+	INSERT INTO silver.crm_prd_info (
+	  prd_id,
+	  cat_id,
+	  prd_key,
+	  prd_nm,
+	  prd_cost,
+	  prd_line,
+	  prd_start_date,
+	  prd_end_date
+	)
+	SELECT 
+	  prd_id,
+	  REPLACE(SUBSTRING(prd_key FROM 1 FOR 5), '-', '_') AS cat_id,
+	  SUBSTRING(prd_key FROM 7) AS prd_key,
+	  prd_nm,
+	  COALESCE(prd_cost, 0) AS prd_cost,
+	  CASE 
+	    WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
+	    WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
+	    WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
+	    WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
+	    ELSE 'n/a'
+	  END AS prd_line,
+	  prd_start_date,
+	  LEAD(prd_start_date) OVER (PARTITION BY prd_key ORDER BY prd_start_date) - 1 AS prd_end_date
+	FROM bronze.crm_prd_info;
+	
+	INSERT INTO silver.crm_sales_details (
+		sls_ord_num ,
+		sls_prd_key ,
+		sls_cust_id ,
+		sls_order_dt,
+		sls_ship_dt,
+		sls_due_dt,
+		sls_sales ,
+		sls_quantity,
+		sls_price 
+	)
+	SELECT
+	  sls_ord_num,
+	  sls_prd_key,
+	  sls_cust_id,
+	  CASE 
+	    WHEN sls_order_dt = '0' OR LENGTH(sls_order_dt :: TEXT) != 8 THEN NULL
+	    ELSE TO_DATE(sls_order_dt :: TEXT, 'YYYYMMDD')
+	  END AS sls_order_dt,
+	  CASE 
+	    WHEN sls_ship_dt = '0' OR LENGTH(sls_ship_dt :: TEXT) != 8 THEN NULL
+	    ELSE TO_DATE(sls_ship_dt :: TEXT, 'YYYYMMDD')
+	  END AS sls_ship_dt,
+	  CASE 
+	    WHEN sls_due_dt = '0' OR LENGTH(sls_due_dt :: TEXT) != 8 THEN NULL
+	    ELSE TO_DATE(sls_due_dt :: TEXT, 'YYYYMMDD')
+	  END AS sls_due_dt,
+	  CASE WHEN sls_sales ISNULL OR sls_sales<=0 OR sls_sales != sls_quantity * ABS(sls_price)  -- ABS CONVERT THE NEG. VALUE TO POS.
+	  	   THEN sls_quantity * ABS(sls_price)
+	       ELSE sls_sales -- Recalcalculate sales if original value is missing or inicorrect
+	  END AS sls_sales,
+	  sls_quantity,
+	  CASE WHEN sls_price IS NULL OR sls_price <=0
+	       THEN sls_sales / NULLIF (sls_quantity, 0)
+		   ELSE sls_price  -- Derive price if the original value is invalid
+	  END AS sls_price
+	FROM bronze.crm_sales_details;
+	
+	-- Now another table changes done below
+	
+	INSERT INTO silver.erp_cust_az12 (cid, bdate, gen)
+	
+	SELECT
+		  	CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(CID, 4, LENGTH(cid))
+			     ELSE cid
+		    END AS cid,
+			CASE WHEN bdate > CURRENT_DATE THEN NULL
+			     ELSE bdate
+			END AS bdate,
+			CASE WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+			     WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+				 ELSE 'n/a'
+			END AS gen
+	FROM bronze.erp_cust_az12;
+	
+	-- nOW THE another table is Remaining
+	INSERT INTO silver.erp_loc_a101 (cid, cntry)
+	
+	SELECT 
+		  REPLACE (cid, '-', '') cid,
+		  CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+		       WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+			   WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+			   ELSE TRIM(cntry)
+		  END AS cntry
+	FROM bronze.erp_loc_a101;
+	
+	-- Now in the last table the data quality is good so their is no need to do data transformation
+	INSERT INTO silver.erp_px_cat_g1v2 
+	(id, cat, subcat, maintanance)
+	
+	SELECT
+		  id,
+		  cat,
+		  subcat,
+		  maintanance
+	FROM bronze.erp_px_cat_g1v2;
+END;
+$$;
